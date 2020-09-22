5fe0a6c02d66b2bc638bc9de7bb8ff78ede8e732
shahine <shahine96@gmail.com>
2020-09-22 23:24:23 +0200

    filtering black frames

--- src/datasets/cloud_top_to_rgb.py
+++ src/datasets/cloud_top_to_rgb.py
@@ -19,11 +19,11 @@ class CloudTOPtoRGBDataset(Dataset):
         self.cloud_top_dataset = np.load(cloud_top_path)
         self.true_color_dataset = np.load(true_color_path)
         ########
-        # threshold = 0.01
-        # fraction_of_null_pixels = np.all(self.true_color_dataset == 0., axis=-1).mean(axis=(1, 2))
-        # valid_samples = fraction_of_null_pixels < threshold
-        # self.cloud_top_dataset = self.cloud_top_dataset[valid_samples]
-        # self.true_color_dataset = self.true_color_dataset[valid_samples]
+        threshold = 0.01
+        fraction_of_null_pixels = np.all(self.true_color_dataset == 0., axis=-1).mean(axis=(1, 2))
+        valid_samples = fraction_of_null_pixels < threshold
+        self.cloud_top_dataset = self.cloud_top_dataset[valid_samples]
+        self.true_color_dataset = self.true_color_dataset[valid_samples]
         ########
 
     def __getitem__(self, idx):
