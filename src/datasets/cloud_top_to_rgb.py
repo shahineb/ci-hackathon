02f75a8d4b7841cfab3f67fb1d2db488855d1bbb
Sofija <sof.stefanovic@gmail.com>
2020-09-24 12:13:04 +0200

    adding normalisation for dataset without dark frames

--- src/datasets/cloud_top_to_rgb.py
+++ src/datasets/cloud_top_to_rgb.py
@@ -16,24 +16,29 @@ class CloudTOPtoRGBDataset(Dataset):
     def _load_datasets(self):
         cloud_top_path = os.path.join(self.root, "X_train_CI20.npy")
         true_color_path = os.path.join(self.root, "Y_train_CI20.npy")
+
         self.cloud_top_dataset = np.load(cloud_top_path)
         self.true_color_dataset = np.load(true_color_path)
-        ########
-        threshold = 0.01
-        fraction_of_null_pixels = np.all(self.true_color_dataset == 0., axis=-1).mean(axis=(1, 2))
-        valid_samples = fraction_of_null_pixels < threshold
-        self.cloud_top_dataset = self.cloud_top_dataset[valid_samples]
-        self.true_color_dataset = self.true_color_dataset[valid_samples]
-        ########
+
+        self.cloud_top_means = np.load("data_stats/means_CT_norm.npy")
+        self.cloud_top_stds = np.load("data_stats/stds_CT_norm.npy")
+        self.true_color_means = np.load("data_stats/means_TC_norm.npy")
+        self.true_color_stds = np.load("data_stats/stds_TC_norm.npy")
+
 
     def __getitem__(self, idx):
         # Load frames
         cloud_top = self.cloud_top_dataset[idx]
         true_color = self.true_color_dataset[idx]
 
-        # Convert to tensors
-        cloud_top = self.transform(cloud_top)
-        true_color = self.transform(true_color)
+        ct_transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize(mean=self.cloud_top_means, std=self.cloud_top_stds)])
+        true_color_transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize(mean=self.true_color_means, std=self.true_color_stds)])
+
+        # Convert to tensor and normalise
+        cloud_top = ct_transform(cloud_top)
+        true_color = true_color_transform(true_color)
 
         # Apply random flip augmentation
         if random.random() < 0.5:
@@ -43,7 +48,14 @@ class CloudTOPtoRGBDataset(Dataset):
         if random.random() < 0.5:
             cloud_top = F.vflip(cloud_top)
             true_color = F.vflip(true_color)
+
+        # Apply random crop (only if converted to PIL image - check Compose)
+        #if random.random() < 0.5:
+        #    cloud_top = F.RandomCrop(cloud_top)
+        #    true_color = F.RandomCrop(true_color)
+
         return cloud_top, true_color
+
 
     def __len__(self):
         return len(self.cloud_top_dataset)
